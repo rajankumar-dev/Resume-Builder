423cddab32e4acbdc698009468dc1b0631f5ae88
rajankumar-dev <rk1620703@gmail.com>
2026-02-15 09:42:47 +0530

    initigrate tailwind in app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,13 @@
 
-import './App.css'
+
 
 function App() {
 
   return (
     <>
-      <h1>App</h1>
+      <h1 className="text-3xl font-bold underline">
+    Hello world!
+  </h1>
     </>
   )
 }
