cae2729537764237d5ab7e26f798d7b1223cd810
rajankumar-dev <rk1620703@gmail.com>
2026-02-16 16:04:08 +0530

    add db connection

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import cors from 'cors'
 import 'dotenv/config'
+import { connectDb } from './config/db.js';
 
 const app = express();
 const PORT = 4000;
@@ -8,13 +9,15 @@ const PORT = 4000;
 app.use(cors())
 
 //connect DB 
-
+connectDb()
 //Middleware
 app.use(express.json())
 
 //route
 app.get('/', (req, res) => {
     res.send('Api working')
+    // console.log(process.env.MONGO_URL);
+
 })
 
 app.listen(PORT, () => {
