ac005026463eca6127538251e64db9a296ef1656
rajankumar-dev <rk1620703@gmail.com>
2026-02-19 10:15:04 +0530

    add new route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,14 @@
+import { Route, Routes } from "react-router-dom"
+import LandingPage from "./pages/LandingPage"
 
 
 
 function App() {
 
   return (
-    <>
-      <h1 className="text-3xl font-bold underline">
-    Hello world!
-  </h1>
-    </>
+    <Routes>
+      <Route path='/' element={<LandingPage />} />
+    </Routes>
   )
 }
 
