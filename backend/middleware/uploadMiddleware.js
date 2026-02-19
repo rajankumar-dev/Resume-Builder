d1f3a8d02887f0d1fe21dd22f8f94c6a12c648ac
rajankumar-dev <rk1620703@gmail.com>
2026-02-19 09:02:43 +0530

    add new middleware func

---
+++ backend/middleware/uploadMiddleware.js
@@ -0,0 +1,22 @@
+import multer from "multer";
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'uploads/')
+    },
+    filename: function (req, file, cb) {
+        cb(null, `${Date.now()}-${file.originalname}`)
+    },
+});
+//file filter to allow only images
+const fileFilter = (req, file, cb) => {
+    const allowedTypes = ['image/jpeg', 'image/png', 'image/gif'];
+    if (allowedTypes.includes(file.mimetype)) {
+        cb(null, true);
+    } else {
+        cb(new Error('Invalid file type. Only JPEG, PNG and GIF are allowed.'), false);
+    }
+}
+const upload = multer({ storage, fileFilter });
+
+export default upload;
