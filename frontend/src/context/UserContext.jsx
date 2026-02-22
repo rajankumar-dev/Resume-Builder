909edb4e8947a18a34f49b2345ad26c8f55ba165
rajankumar-dev <rk1620703@gmail.com>
2026-02-22 21:53:08 +0530

    usercontext

---
+++ frontend/src/context/UserContext.jsx
@@ -0,0 +1,34 @@
+import { createContext, useEffect, useState } from 'react'
+
+export const UserContext = createContext()
+
+const UserProvider = ({ children }) => {
+    const [user, setUser] = useState(null);
+    const [loading, setLoding] = useState(true);
+
+    useEffect(() => {
+        if (user) return
+
+        const accessToken = localStorage.getItem('token')
+        if (!accessToken) {
+            setLoding(false)
+            return
+        }
+
+        const fetchUser = async () => {
+            try {
+                const response = await axiosInstance.get(API_PATHS.auth.get_profile)
+                setUser(response.data)
+            } catch (error) {
+                console.error('Error fetching user profile:', error)
+                localStorage.removeItem('token')
+            } finally {
+                setLoding(false)
+            }
+        }
+
+        fetchUser()
+
+    })
+
+}
