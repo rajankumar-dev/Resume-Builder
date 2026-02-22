3a454098bd086522fc29344db6a8d0e9f2fa3e38
rajankumar-dev <rk1620703@gmail.com>
2026-02-22 21:53:26 +0530

    load data to the client

---
+++ frontend/src/pages/LandingPage.jsx
@@ -0,0 +1,39 @@
+import { LayoutTemplate, Menu, X } from 'lucide-react';
+import { landingPageStyles } from '../assets/dummystyles.js';
+import { useState } from 'react';
+
+const LandingPage = () => {
+
+  const [MobileMenuOpen, setMobileMenuOpen] = useState(false);
+
+  return (
+    <div className={landingPageStyles.container}>
+      {/*HEADER*/}
+      <header className={landingPageStyles.header}>
+        <div className={landingPageStyles.headerContainer}>
+          <div className={landingPageStyles.logoContainer}>
+            <div className={landingPageStyles.logoIcon}>
+              <LayoutTemplate className={landingPageStyles.logoIconInner} />
+            </div>
+            <span className={landingPageStyles.logoText}>ResumeExpert</span>
+          </div>
+          {/*Mobile Menu btn*/}
+          <button className={landingPageStyles.mobileMenuButton}
+            onClick={() => setMobileMenuOpen(!MobileMenuOpen)}>
+            {MobileMenuOpen ?
+              <X size={24} className={landingPageStyles.mobileMenuIcon} /> :
+              <Menu size={24} className={landingPageStyles.mobileMenuIcon} />}
+          </button>
+          {/*Desktop Navigation Menu*/}
+          <div className='hidden md:flex items-center'>
+            {user}
+          </div>
+        </div>
+      </header>
+    </div>
+  )
+
+
+};
+
+export default LandingPage;
