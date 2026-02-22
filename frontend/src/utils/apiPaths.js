a08e7d32dfb09ee64e206d7e6e18d59255fa42bd
rajankumar-dev <rk1620703@gmail.com>
2026-02-22 21:52:00 +0530

    create path into client

---
+++ frontend/src/utils/apiPaths.js
@@ -0,0 +1,21 @@
+export const BASE_URL = "http://localhost:4000";
+
+//Routes
+export const API_PATHS = {
+  auth: {
+    login: "/api/auth/login",
+    register: "/api/auth/register",
+    get_profile: "/api/auth/profile",
+  },
+  resume: {
+    create: "/api/resumes",
+    get_all: "/api/resumes",
+    get_by_id: (id) => `/api/resumes/${id}`,
+    update: (id) => `/api/resumes/${id}`,
+    delete: (id) => `/api/resumes/${id}`,
+    upload_image: (resumeId) => `/api/resumes/${resumeId}/upload-image`,
+  },
+  image: {
+    upload_image: `/api/images/upload`,
+  },
+};
